42e5609679d1c5f09aa30419222f45d492016dfc
Diogo Marcondes <diogo.marcondes@hotmail.com>
2023-02-09 23:18:24 -0300

    Primeira Infra

---
+++ ec2.tf
@@ -0,0 +1,71 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "terraform-1" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  key_name = "diogo" 
+  subnet_id = var.subnet_public_id
+  vpc_security_group_ids = [aws_security_group.permitir_ssh_http.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "Terraform-01"
+ # Insira o nome da instância de sua preferência.
+  }
+}
+
+variable "vpc_id" {
+  default = "vpc-0303fb420f519ea05" # Sua VPC
+}
+
+variable "subnet_public_id" {
+  default = "subnet-0b46358b7f41a0ce5" # Sua Subnet da VPC
+}
+
+
+resource "aws_security_group" "permitir_ssh_http" {
+  name        = "permitir_ssh"
+  description = "Permite SSH e HTTP na instancia EC2"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH to EC2"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP to EC2"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "permitir_ssh_e_http"
+  }
+}
---
+++ provider.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+    region = "us-east-1"
+    access_key = "AKIA2FRKKWVJPFNQYWP7"
+    secret_key = "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"
+}
+
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-pa"
+#    key    = "terraform.tfstate"
+#    region = "us-east-1"
+#  }
+#}
+
+
