c9d524dd93e16068a9cc131e83ddc3e077dd8df2
Diogo Marcondes <diogo.marcondes@hotmail.com>
2023-02-11 13:17:37 -0300

    correcao

--- provider.tf
+++ provider.tf
@@ -1,7 +1,5 @@
 provider "aws" {
     region = "us-east-1"
-    access_key = "AKIA2FRKKWVJMI45XUXG"
-    secret_key = "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"
 }
 
 terraform {
@@ -11,5 +9,6 @@ terraform {
     region = "us-east-1"
   }
 }
+
 
 
